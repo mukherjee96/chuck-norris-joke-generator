4c36c49ad6ef63f3fa3d0a56a48a01ea70ed2f21
Aritra Mukherjee <40653099+mukherjee96@users.noreply.github.com>
2019-04-22 20:38:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ function getJokes(e) {
     e.target.innerText = 'Generating..';
     const jokes = document.getElementById('jokes');
     const number = document.getElementById('number').value;
-    fetch(`http://api.icndb.com/jokes/random/${number}`).then(res => res.json()).then(data => {
+    fetch(`https://api.icndb.com/jokes/random/${number}`).then(res => res.json()).then(data => {
         let result = '';
         data.value.forEach(joke => {
             result += `<li>${joke.joke}</li>`;
         });
         jokes.innerHTML = result;
         e.target.innerText = 'Get Jokes!';
-    }).catch(err => jokes.innerHTML = '<li>Something went wrong. Try again!</li>');
+    }).catch(err => {
+        jokes.innerHTML = '<li>Something went wrong. Try again!</li>';
+        e.target.innerText = 'Get Jokes!';
+    });
 
     e.preventDefault();
 }
